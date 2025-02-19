0a7b627e7f3c48e3c869be463547ec6d3c55cf7e
ndiagar2111 <ndiagar2111@g.educaand.es>
2025-02-19 12:27:46 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 basic.forever(function () {
     if (Maqueen_V5.Ultrasonic() < 10) {
         Maqueen_V5.motorStop(Maqueen_V5.Motors.M1)
-        Maqueen_V5.motorRun(Maqueen_V5.Motors.M2, Maqueen_V5.Dir.CCW, 100)
-        basic.pause(600)
+        Maqueen_V5.motorRun(Maqueen_V5.Motors.M2, Maqueen_V5.Dir.CCW, 148)
+        basic.pause(700)
     }
-    Maqueen_V5.motorRun(Maqueen_V5.Motors.All, Maqueen_V5.Dir.CW, 100)
+    Maqueen_V5.motorRun(Maqueen_V5.Motors.All, Maqueen_V5.Dir.CW, 255)
 })
